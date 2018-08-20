c4c3f719f6899e245d05dc9b9e18dfb4ef70dbe5
TADAS-PC\taaas <tadas.mockvicius@gmail.com>
2018-08-20 13:54:41 +0300

    User sessions

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -1,23 +1,19 @@
 <?php
-
-class Users extends Controller
-{
-    public function __construct()
-    {
-
+class Users extends Controller {
+    public function __construct(){
+        $this->userModel = $this->model('User');
     }
 
-    public function register()
-    {
+    public function register(){
         // Check for POST
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
             // Process form
 
             // Sanitize POST data
             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
             // Init data
-            $data = [
+            $data =[
                 'name' => trim($_POST['name']),
                 'email' => trim($_POST['email']),
                 'password' => trim($_POST['password']),
@@ -29,35 +25,51 @@ class Users extends Controller
             ];
 
             // Validate Email
-            if (empty($data['email'])) {
+            if(empty($data['email'])){
                 $data['email_err'] = 'Please enter email';
+            } else {
+                // Check email
+                if($this->userModel->findUserByEmail($data['email'])){
+                    $data['email_err'] = 'Email is already taken';
+                }
             }
 
             // Validate Name
-            if (empty($data['name'])) {
+            if(empty($data['name'])){
                 $data['name_err'] = 'Please enter name';
             }
 
             // Validate Password
-            if (empty($data['password'])) {
+            if(empty($data['password'])){
                 $data['password_err'] = 'Please enter password';
-            } elseif (strlen($data['password']) < 6) {
+            } elseif(strlen($data['password']) < 6){
                 $data['password_err'] = 'Password must be at least 6 characters';
             }
 
             // Validate Confirm Password
-            if (empty($data['confirm_password'])) {
+            if(empty($data['confirm_password'])){
                 $data['confirm_password_err'] = 'Please confirm password';
             } else {
-                if ($data['password'] != $data['confirm_password']) {
+                if($data['password'] != $data['confirm_password']){
                     $data['confirm_password_err'] = 'Passwords do not match';
                 }
             }
 
             // Make sure errors are empty
-            if (empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])) {
+            if(empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])){
                 // Validated
-                die('SUCCESS');
+
+                // Hash Password
+                $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+
+                // Register User
+                if($this->userModel->register($data)){
+                    flash('register_success', 'You are registered and can log in');
+                    redirect('users/login');
+                } else {
+                    die('Something went wrong');
+                }
+
             } else {
                 // Load view with errors
                 $this->view('users/register', $data);
@@ -65,7 +77,7 @@ class Users extends Controller
 
         } else {
             // Init data
-            $data = [
+            $data =[
                 'name' => '',
                 'email' => '',
                 'password' => '',
@@ -81,16 +93,15 @@ class Users extends Controller
         }
     }
 
-    public function login()
-    {
+    public function login(){
         // Check for POST
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
             // Process form
             // Sanitize POST data
             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
             // Init data
-            $data = [
+            $data =[
                 'email' => trim($_POST['email']),
                 'password' => trim($_POST['password']),
                 'email_err' => '',
@@ -98,19 +109,37 @@ class Users extends Controller
             ];
 
             // Validate Email
-            if (empty($data['email'])) {
-                $data['email_err'] = 'Pleae enter email';
+            if(empty($data['email'])){
+                $data['email_err'] = 'Please enter email';
             }
 
             // Validate Password
-            if (empty($data['password'])) {
+            if(empty($data['password'])){
                 $data['password_err'] = 'Please enter password';
             }
 
+            // Check for user/email
+            if($this->userModel->findUserByEmail($data['email'])){
+                // User found
+            } else {
+                // User not found
+                $data['email_err'] = 'No user found';
+            }
+
             // Make sure errors are empty
-            if (empty($data['email_err']) && empty($data['password_err'])) {
+            if(empty($data['email_err']) && empty($data['password_err'])){
                 // Validated
-                die('SUCCESS');
+                // Check and set logged in user
+                $loggedInUser = $this->userModel->login($data['email'], $data['password']);
+
+                if($loggedInUser){
+                    // Create Session
+                    $this->createUserSession($loggedInUser);
+                } else {
+                    $data['password_err'] = 'Password incorrect';
+
+                    $this->view('users/login', $data);
+                }
             } else {
                 // Load view with errors
                 $this->view('users/login', $data);
@@ -119,7 +148,7 @@ class Users extends Controller
 
         } else {
             // Init data
-            $data = [
+            $data =[
                 'email' => '',
                 'password' => '',
                 'email_err' => '',
@@ -128,6 +157,29 @@ class Users extends Controller
 
             // Load view
             $this->view('users/login', $data);
+        }
+    }
+
+    public function createUserSession($user){
+        $_SESSION['user_id'] = $user->id;
+        $_SESSION['user_email'] = $user->email;
+        $_SESSION['user_name'] = $user->name;
+        redirect('pages/index');
+    }
+
+    public function logout(){
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_email']);
+        unset($_SESSION['user_name']);
+        session_destroy();
+        redirect('users/login');
+    }
+
+    public function isLoggedIn(){
+        if(isset($_SESSION['user_id'])){
+            return true;
+        } else {
+            return false;
         }
     }
 }
--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,8 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
     <div class="container">
         <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
-                aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -15,13 +14,20 @@
                     <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
                 </li>
             </ul>
+
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">Register</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
-                </li>
+                <?php if(isset($_SESSION['user_id'])) : ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
+                    </li>
+                <?php else : ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
+                    </li>
+                <?php endif; ?>
             </ul>
         </div>
     </div>
